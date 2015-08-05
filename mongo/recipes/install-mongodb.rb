be9089d620fcfcb5f020213321006296820075ff
mitchross <mitchross09@gmail.com>
2015-08-04 20:30:08 -0400

    clean up script

--- mongo/recipes/install-mongodb.rb
+++ mongo/recipes/install-mongodb.rb
@@ -1,15 +1,16 @@
-bash "install mongodb" do
-user "root"
-code <<-EOH
-echo "Installing MongoDB" &&
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 &&
-echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list &&
-apt-get -y update &&
-# Install MongoDB 2.6, not the old 2.4 installed by mongodb-10gen
-apt-get install -y mongodb-org &&
-echo "Installed MongoDB"
-echo "start Mongo"
-stop mongod
-sudo service mongod start
-EOH
+bash "install_mongodb_custom" do
+	interpreter "bash"
+	user "root"
+	code <<-EOH
+		echo "Installing MongoDB" &&
+		apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 &&
+		echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list &&
+		apt-get -y update &&
+		# Install MongoDB 2.6, not the old 2.4 installed by mongodb-10gen
+		apt-get install -y mongodb-org &&
+		echo "Installed MongoDB"
+		echo "start Mongo"
+		stop mongod
+		sudo service mongod start
+	EOH
 end
