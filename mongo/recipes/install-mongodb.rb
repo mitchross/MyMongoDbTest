789d4755b0a59d7d15016f8b6da4677b59ff6696
mitchross <mitchross09@gmail.com>
2015-08-04 22:50:01 -0400

    new script update

--- mongo/recipes/install-mongodb.rb
+++ mongo/recipes/install-mongodb.rb
@@ -3,16 +3,15 @@ bash "install_mongodb_custom" do
 	user "root"
 	code <<-EOH
 		echo "Installing MongoDB" &&
-		apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 &&
-		echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list &&
-		apt-get -y update &&
-		# Install MongoDB 2.6, not the old 2.4 installed by mongodb-10gen
-		apt-get install -y mongodb-org &&
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 &&
+		echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list &&
+		sudo apt-get update &&
+		sudo apt-get install mongodb-10gen &&
 		echo "Installed MongoDB"
 
 		sudo sed -i 's/bind_ip = 127.0.0.1/bind_ip = 0.0.0.0/' /etc/mongod.conf &&
 		echo "start Mongo"
-		stop mongod
+		sudo service mongodb stop
 		sudo service mongod -f /etc/mongodb.conf start
 	EOH
 end
