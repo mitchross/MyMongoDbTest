bf4b155e72a92ac8b41daa29961742578d597e11
mitchross <mitchross09@gmail.com>
2015-08-04 23:13:45 -0400

    add assume yes

--- mongo/recipes/install-mongodb.rb
+++ mongo/recipes/install-mongodb.rb
@@ -5,8 +5,8 @@ bash "install_mongodb_custom" do
 		echo "Installing MongoDB" &&
 		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 &&
 		echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list &&
-		sudo apt-get update &&
-		sudo apt-get install mongodb-10gen &&
+		sudo apt-get -y update &&
+		sudo apt-get -y install mongodb-10gen &&
 		echo "Installed MongoDB"
 
 		sudo sed -i 's/bind_ip = 127.0.0.1/bind_ip = 0.0.0.0/' /etc/mongod.conf &&
