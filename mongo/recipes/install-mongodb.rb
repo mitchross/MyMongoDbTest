d369b0621b64f2ac206638f1caa64d1316d6c356
mitchross <mitchross09@gmail.com>
2015-08-18 23:01:06 -0400

    try another stderr ignore

--- mongo/recipes/install-mongodb.rb
+++ mongo/recipes/install-mongodb.rb
@@ -4,8 +4,8 @@ bash "install_mongodb_custom" do
 	code <<-EOH
 		echo "Installing MongoDB" &&
 		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 2>&1 &&
-		echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list &&
-		sudo apt-get -y update &&
+		echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list 2>&1 &&
+		sudo apt-get -y update && 
 		sudo apt-get -y install mongodb-10gen &&
 		echo "Installed MongoDB"
 
